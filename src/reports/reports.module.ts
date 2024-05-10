a63dec6a16228e0623497e9d0e9f80cd03dc1979
Sideras1226 <traxjohn510@gmail.com>
2024-05-10 14:38:08 +0300

    create reports controller

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,3 +1,7 @@
 import { Module } from '@nestjs/common';
-@Module({})
+import { ReportsController } from './reports.controller';
+
+@Module({
+  controllers: [ReportsController],
+})
 export class ReportsModule {}
