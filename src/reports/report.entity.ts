20b0b5ea9a43131ee3eed565d84ac5b40eddef91
Sideras1226 <traxjohn510@gmail.com>
2024-05-10 14:46:24 +0300

    Remove unused import

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToMany } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity('reports')
 export class Report {
