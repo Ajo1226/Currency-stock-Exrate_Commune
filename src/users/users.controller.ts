e73813ea1b262bb377fb0d4fd5cefe31488da6d1
Sideras1226 <traxjohn510@gmail.com>
2024-05-10 19:38:29 +0300

    Apply UserDTO serialization on the whole USer controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,7 +12,10 @@ import {
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
+@Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
   constructor(private usersService: UsersService) {}
