50042fcebdbc0f9f3a8a4a43fe52afdb83237233
Sideras1226 <traxjohn510@gmail.com>
2024-05-10 17:31:57 +0300

    Add logs after insert, update and remove in user entity

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,11 +1,35 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity('users')
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
+
   @Column()
   email: string;
+
   @Column()
   password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted user with id: ', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated user with id: ', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed user with id: ', this.id);
+  }
 }
