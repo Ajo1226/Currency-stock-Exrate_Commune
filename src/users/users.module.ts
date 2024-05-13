fcd8f7e7a80503d9f9bb4288b4beba253b2bc3e3
Sideras1226 <traxjohn510@gmail.com>
2024-05-13 13:04:10 +0300

    Apply CurrentUserInterceptor globally scoped for the app

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,10 +4,16 @@ import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { AuthService } from './auth.service';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService, AuthService],
+  providers: [
+    UsersService,
+    AuthService,
+    { provide: APP_INTERCEPTOR, useClass: CurrentUserInterceptor },
+  ],
 })
 export class UsersModule {}
