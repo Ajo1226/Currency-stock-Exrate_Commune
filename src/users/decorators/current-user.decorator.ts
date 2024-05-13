89ab3a0ddb5747bd2ca59177759832075eb39334
Sideras1226 <traxjohn510@gmail.com>
2024-05-13 12:59:35 +0300

    Create a decorator for getting the current user for the http request

---
+++ src/users/decorators/current-user.decorator.ts
@@ -0,0 +1,8 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const CurrentUser = createParamDecorator(
+  (data: never, context: ExecutionContext) => {
+    return context.switchToHttp().getRequest().currentUser;
+  },
+);
+x;
