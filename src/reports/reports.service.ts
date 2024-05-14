38880e2217de4159acd09fe717189238a132dd24
Sideras1226 <traxjohn510@gmail.com>
2024-05-14 12:37:22 +0300

    Implement new function for creating new reports in reports.service.ts

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Report } from './report.entity';
+import { Repository } from 'typeorm';
+import { User } from '../users/user.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+  create(user: User, reportData: CreateReportDto) {
+    const report = this.repo.create(reportData);
+    report.user = user;
+    return this.repo.save(report);
+  }
+}
