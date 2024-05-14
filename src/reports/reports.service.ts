7af0d79977f0c861a392a3af780577efe9d22b0f
Sideras1226 <traxjohn510@gmail.com>
2024-05-14 18:13:21 +0300

    Implement estimation function in report service

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -4,10 +4,28 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Report } from './report.entity';
 import { Repository } from 'typeorm';
 import { User } from '../users/user.entity';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Injectable()
 export class ReportsService {
   constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+  createEstimate({ make, model, lng, lat, year, mileage }: GetEstimateDto) {
+    return this.repo
+      .createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where('make = :make ', { make })
+      .andWhere('model = :model ', { model })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
+      .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
+      .andWhere('year - :year BETWEEN -3 AND 3', { year })
+      .andWhere('approved IS TRUE')
+      .orderBy('ABS(mileage - :mileage)', 'DESC')
+      .setParameters({ mileage })
+      .limit(3)
+      .getRawOne();
+  }
+
   create(user: User, reportData: CreateReportDto) {
     const report = this.repo.create(reportData);
     report.user = user;
