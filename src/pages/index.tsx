8479d108d92889c45c046a044b781cc536ecd0bb
Sideras1226 <traxjohn510@gmail.com>
2024-05-16 02:48:10 +0900

    fix_addFirstpage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,25 @@
+import { Flex } from "@mantine/core"
 import { GetServerSideProps } from "next"
+import Link from "next/link"
 
 export default function Home() {
-	return <></>
+	return (
+        <>
+			<div style={{display: "flex", justifyContent:"center",  alignItems:"center"}}>
+				<Link href="/dashboard">
+					<button style={{fontSize:"30px", margin:"30px", padding:"10px", borderRadius:"10px"}}>Get started</button>
+				</Link>
+			</div>
+        </>
+    )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
-	return {
-		redirect: {
-			destination: "/dashboard",
-		},
-		props: {},
-	}
-}
+
+// export const getServerSideProps: GetServerSideProps = async () => {
+// 	return {
+// 		redirect: {
+// 			destination: "/dashboard",
+// 		},
+// 		props: {},
+// 	}
+// }
