e9f7961e16841f0860e71b748d484b65209acdf2
Sideras1226 <traxjohn510@gmail.com>
2024-05-16 17:28:08 +0900

    fix_firstpage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,20 @@
-import { Flex } from "@mantine/core"
+import { Button, Flex } from "@mantine/core"
 import { GetServerSideProps } from "next"
 import Link from "next/link"
 
 export default function Home() {
 	return (
         <>
-			<div style={{display: "flex", justifyContent:"center",  alignItems:"center"}}>
+			<div style={{display: "flex", flexDirection:"column", justifyContent:"center", alignItems:"center", padding:"50px"}}>
+				<img src="/logo.gif" style={{height:"400px", width:"400px", marginBottom:"30px"}}></img>
+				<div style={{container:"revert", textAlign:"center", marginBottom:"30px"}}>
+					<h3>
+						This is a new idea to get Stock prices, Stock insights, Forex prices, Forex insights, and News related to the markets.<br></br>
+						Users also can bookmark their favorite Stock companies and Forex pairs to keep track of them.
+					</h3>
+				</div>
 				<Link href="/dashboard">
-					<button style={{fontSize:"30px", margin:"30px", padding:"10px", borderRadius:"10px"}}>Get started</button>
+					<Button style={{fontSize:"20px", height:"40px"}}>Let's Go</Button>
 				</Link>
 			</div>
         </>
